d2016f10412d0c74156ccf55cda8c2b4f72446fe
mstudham <michael.studham@btinternet.com>
2021-12-10 10:21:44 +0000

    ag-grid ws initial

--- customization-demo/src/app/number-formatter.component.ts
+++ customization-demo/src/app/number-formatter.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-number-formatter-cell',
   template: `
-    <span>{{params.value | currency:'EUR'}}</span>
+    <span>{{params.value | currency:'GBP'}}</span>
   `
 })
 export class NumberFormatterComponent {
